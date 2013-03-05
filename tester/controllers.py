0b6dbed7bd5505bb2cb735c157c2572552218a39
Ed Finkler <coj@funkatron.com>
2013-03-05 11:47:18 -0500

    add ability to send an email; clean up forms a bit

--- tester/controllers.py
+++ tester/controllers.py
@@ -1,7 +1,7 @@
 # app is instanced in __init__.py; this gets it from there
 from . import app
-from .utils import render_mandrill_template, get_templates
-from flask import render_template, request
+from .utils import render_mandrill_template, get_templates, send_mail_mandrill
+from flask import render_template, request, abort
 import json
 
 
@@ -15,13 +15,35 @@ def index():
 
 @app.route('/render/', methods=['POST'])
 def render():
+    """
+    renders and/or sends!
+    """
+    app.logger.debug(request.form)
+    action = request.form.get('action')
     template_slug = request.form.get('template-slug')
     template_content = json.loads(request.form.get('template-content'))
     merge_vars = json.loads(request.form.get('merge-vars'))
-    html = render_mandrill_template(template_slug,
-                                    template_content=template_content,
-                                    merge_vars=merge_vars)
-    return html
+
+    app.logger.debug(action)
+
+    if (action == 'render'):
+        html = render_mandrill_template(template_slug,
+                                        template_content=template_content,
+                                        merge_vars=merge_vars)
+        return html
+
+    if (action == 'send'):
+        to_email = request.form.get('to-email')
+        from_email = request.form.get('from-email')
+        subject = request.form.get('subject')
+        resp = send_mail_mandrill(template_slug, to_email, subject,
+                                  from_address=from_email,
+                                  template_content=template_content,
+                                  merge_vars=merge_vars)
+        app.logger.debug(resp)
+        return "Mandrill response: %s" % (resp)
+
+    abort(400, "Bad action")
 
 
 @app.route('/template/<slug>', methods=['GET'])
--- tester/utils.py
+++ tester/utils.py
@@ -18,5 +18,35 @@ def get_templates(slug=None):
 
 def render_mandrill_template(slug, template_content=None, merge_vars=None):
     md = _get_mandrill()
-    resp = md.templates.render(slug, template_content, merge_vars=merge_vars)
+    resp = md.templates.render(slug, template_content=None,
+                               merge_vars=merge_vars)
     return resp['html']
+
+
+def send_mail_mandrill(slug, to, subject, from_address='support@donenotdone.com',
+                       from_name="Done Not Done", template_content=None,
+                       merge_vars=None):
+    """
+    send an email using Mandrill
+    """
+    if not from_address:
+        from_address = app.config['FEEDBACK_EMAIL'][0]
+
+    message = {
+        "track_opens": True,
+        "track_clicks": True,
+        'subject': subject,
+        "from_email": from_address,
+        "from_name": from_name,
+        "to": [
+            {
+                "email": to,
+            }
+        ],
+        "global_merge_vars": merge_vars,
+    }
+
+    md = _get_mandrill()
+    resp = md.messages.send_template(slug, template_content, message, async=True)
+
+    return resp
