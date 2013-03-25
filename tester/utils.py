3d4e97aaf71b4831673e7d85b557e1224912b285
Ed Finkler <coj@funkatron.com>
2013-03-25 09:46:11 -0400

    add a method to send a raw html email

--- tester/utils.py
+++ tester/utils.py
@@ -50,3 +50,31 @@ def send_mail_mandrill(slug, to, subject, from_address='support@donenotdone.com'
     resp = md.messages.send_template(slug, template_content, message, async=True)
 
     return resp
+
+
+def send_mail_raw_mandrill(to, subject, html, from_address='support@donenotdone.com',
+                           from_name="Done Not Done"):
+    """
+    send an email using Mandrill with no template -- raw html
+    """
+    if not from_address:
+        from_address = app.config['FEEDBACK_EMAIL'][0]
+
+    message = {
+        "track_opens": True,
+        "track_clicks": True,
+        'subject': subject,
+        "from_email": from_address,
+        "from_name": from_name,
+        "to": [
+            {
+                "email": to,
+            }
+        ],
+        "html": html
+    }
+
+    md = _get_mandrill()
+    resp = md.messages.send(message, async=True)
+
+    return resp
